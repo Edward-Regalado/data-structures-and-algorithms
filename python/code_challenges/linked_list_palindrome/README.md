7684f10cd590a0e5b3d118cf20614578bd565fe9
Edward Regalado <tregalado30@gmail.com>
2021-07-15 23:36:25 -0700

    finishing up old code challenges

---
+++ python/code_challenges/linked_list_palindrome/README.md
@@ -0,0 +1,14 @@
+# Code Challenge: 9 Linked List Palindrome
+
+## Challenge
+- Write a function to validate whether or not a linked list is a palindrome.
+
+## Whiteboard
+
+![WhiteBoard](../../assets/WhiteBoard_interview_code_challenge_9.PNG)
+
+
+## Approach & Efficiency
+
+## API
+<!-- Description of each method publicly available to your Stack and Queue-->
--- python/code_challenges/stack_and_queue/stack.py
+++ python/code_challenges/stack_and_queue/stack.py
@@ -1,38 +1,76 @@
-from ..linked_list.linked_list import LinkedList, Node
+class InvalidOperationError(BaseException):
+    pass
+
+class Node:
+        def __init__(self, value, next=None):
+            self.value = value
+            self.next = next
 
 class Stack:
-
-    class Node:
-        def __init__(self, value, next=None):
-            self.value = value
-            self.next = next
-
-    def length(self):
-        pass
+    def __init__(self, node=None):
+        self.top = node
 
     def push(self, value):
-        pass
+        # create a node, pass in value
+        node = Node(value)
+        # this points the new node to top of stack
+        node.next = self.top
+        # assign node to the top of stack
+        self.top = node
 
     def pop(self, value):
-        pass
+        if self.is_empty():
+            raise InvalidOperationError("Method not allowed on empty collection")
+        node = self.top
+        self.top = self.top.next
+        return node.value
 
     def peek(self):
-        pass
+        if self.is_empty():
+            raise InvalidOperationError("Method not allowed on empty collection")
+
+    def is_empty(self):
+        if self.top == None:
+            return True
+        else:
+            return False
 
 class Queue:
-    class Node:
-        def __init__(self, value, next=None):
-            self.value = value
-            self.next = next
+    # First-In-First-Out
+    def __init__(self, node=None):
+        self.front = node
+        self.rear = Node
 
-    def length(self):
-        pass
+    def enqueue(self, value):
+        node = Node(value)
 
-    def push(self, value):
-        pass
+        if self.front is None:
+            self.front = node
+            self.rear = node
+            return self
 
-    def pop(self, value):
-        pass
+        self.rear.next = node
+        self.rear = node
+        return self
+
+    def dequeue(self):
+
+        if self.front is None:
+            return None
+
+        deque = self.front.value
+        self.front = self.front.next
+        return deque
 
     def peek(self):
-        pass
+
+        if self.front is None:
+            return None
+
+        return self.front.value
+
+    def isEmpty(self):
+        return self.front == None
+
+if __name__ == "__main__":
+    pass
