60ef661eddd786b34f5a04d949be600bf4088579
Edward Regalado <tregalado30@gmail.com>
2021-07-26 22:08:00 -0700

    refactored

--- python/code_challenges/trees/tree.py
+++ python/code_challenges/trees/tree.py
@@ -1,9 +1,9 @@
 class Node:
     """
-    Docstring
+    Create a Node which has properties of the value stored in the Node, left and right child nodes.
     """
-    def __init__ (self, data = None, left = None, right = None):
-        self.data = data
+    def __init__ (self, value = None, left = None, right = None):
+        self.value = value
         self.left = left
         self.right = right
 
@@ -11,87 +11,131 @@ class Node:
 
 class BinaryTree:
     """
-    Docstring
+    Create a Binary Class Tree, which has methods to traverse the tree called preorder, inorder, and post order. This will return the values in binary tree in the correct order.
 
     """
     def __init__(self, root = None):
-        self.root = root
+        self.root = Node(root)
 
+     # Preorder Method: Traverse a Binary Tree from root >> left >> right.
+    def pre_order(self):
 
-    def pre_order():
-        # root >> left >> right
         elements = []
 
-        pass
+        # check it root value is empty before running the traverse method, this will return a string and end the pre_order function, else run the traverse method.
+        if self.root is None:
+            return "Empty Tree"
 
-    def in_order(self):
-        elements = []
+        def traverse(root):
 
-        # visit left tree
-        if self.left:
-            elements += self.left.in_order()
+            elements.append(root.value)
+            # If root node has a left value (True), recursively call the traverse method until it returns None.
+            if root.left:
+                traverse(root.left)
+            # If root node has a right value (True), recrusively call the traverse helper method unit it returns None.
+            if root.right:
+                traverse(root.right)
 
-        # visit base node
-        elements.append(self.data)
-
-        # visit rigth tree
-        if self.right:
-            elements += self.left.in_order()
+        traverse(self.root)
 
         return elements
 
-    def post_order():
-        # left >> right >> root
-        pass
 
+    # InOrder Method: Traverse a Binary Tree from left >> root >> right.
+    def in_order(self):
+
+        elements = []
+
+        # check it root value is empty before running the traverse method, this will return a string and end the in_order function, else run the traverse method.
+        if self.root is None:
+            return "Empty Tree"
+
+        def traverse(root):
+            # vist left tree.
+            if root.left:
+                traverse(root.left)
+
+            elements.append(root.value)
+
+        # visit right tree
+            if root.right:
+                traverse(root.right)
+
+        traverse(self.root)
+
+        return elements
+
+
+    # PostOrder Method: Traverse a Binary Tree from left >> right>> root.
+    def post_order(self):
+
+        elements = []
+
+        if self.root is None:
+            return "Empty Tree"
+
+        def traverse(root):
+            if root.left:
+                traverse(root.left)
+
+            if root.right:
+                traverse(root.right)
+
+            elements.apend(root.value)
+
+        traverse(self.root)
+
+        return elements
 
 
 
 class BinarySearchTree(BinaryTree):
     """
-    Docstring
+    Add a node to the binary search tree. BST only contains intergers/numbers.
     """
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, value):
+        self.value = value
         self.left = None
         self.right = None
 
-    def add(self, data):
-        if data == self.data:
-            return Node(val)
+    def add(self, value):
 
-        if data < self.data:
+
+        if value == self.value:
+            return Node(value)
+
+        if value < self.value:
 
             if self.left:
-                self.left.add(data)
+                self.left.add(value)
 
             else:
-                self.left = BinaryTree(data)
+                self.left = BinaryTree(value)
 
         else:
             if self.right:
-                self.right.add(data)
+                self.right.add(value)
             else:
-                self.right = BinaryTree(data)
+                self.right = BinaryTree(value)
 
 
-    def contains(self, val):
-
-        if self.data == val:
+    def contains(self, value):
+        """Search to see if the BST contains a certain value. Returns a boolean to see if value is already inside the tree"""
+        if self.value == value:
             return True
 
-        if val < self.data:
+        if value < self.value:
             if self.left:
-                self.left.contains(val)
+                self.left.contains(value)
             else:
                 return False
 
-        if val > self.data:
+        if value > self.value:
             if self.right:
-                self.right.contains(val)
+                self.right.contains(value)
             else:
                 return False
 
 
-if __name__ == '__main__':
+
 
