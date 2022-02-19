07c058bdb369f4179f364d0fac3f6b922f647923
Eddward-Regalado <tregalado30@gmail.com>
2022-02-18 22:51:38 -0800

    finished functions, not finsihed testing

--- java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
+++ java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
@@ -1,8 +1,9 @@
 package datastructures.linkedlist;
 
 import org.junit.jupiter.api.Test;
+import java.sql.SQLOutput;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LinkedListTest
 {
@@ -10,6 +11,38 @@ public class LinkedListTest
   {
     LinkedList sut = new LinkedList();
     System.out.println(sut);
+    assertTrue(true);
+  }
+
+  @Test void instantiateLinkedList(){
+    LinkedList newLinkedList = new LinkedList();
+    System.out.println(newLinkedList);
+    assertTrue(true);
+  }
+
+  @Test void insertNode(){
+    LinkedList newLinkedList = new LinkedList();
+    newLinkedList.insert(1);
+    System.out.println(newLinkedList.toString());
+    assertTrue(newLinkedList.head.value == 1);
+  }
+
+  @Test void insertMultipleNodes(){
+    LinkedList newLinkedList = new LinkedList();
+    newLinkedList.insert(1);
+    newLinkedList.insert(2);
+    newLinkedList.insert(3);
+    System.out.println(newLinkedList.toString());
+    assertTrue(true);
+  }
+
+  @Test void containsValue(){
+    LinkedList newLinkedList = new LinkedList();
+    newLinkedList.insert(100);
+    assertTrue(newLinkedList.includes(100));
+  }
+
+  @Test void doesNotContainValue(){
     assertTrue(true);
   }
 }
