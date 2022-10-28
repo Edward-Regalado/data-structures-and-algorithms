1bf3e3d6ccb5ca5fe5f42c5fa8e42f4b61894a05
Edward-Regalado <tregalado30@gmail.com>
2022-10-28 06:47:04 +0000

    added right join logic

--- javascript/code-challenges/hashmap-left-join/hashmap-left-join.js
+++ javascript/code-challenges/hashmap-left-join/hashmap-left-join.js
@@ -1,21 +1,37 @@
-function leftJoin(leftMap, rightMap){
+function leftJoin(leftMap, rightMap, direction='left'){
+  const join = direction.toLowerCase();
   const results = [];
+
   // check if we have an empty map, throw an error
   if(!leftMap.size || !rightMap.size){
     throw new Error('empty hashmap!');
   }
   // push a new nested array of key/value pairs into our results array
-  for(const [key, value] of leftMap){
-    results.push([key, value]);
-  }
-  // loop through our results array and check if map2 has the key
-  // if key is found, push the value to our nested array at index[i]
-  for(let i = 0; i < results.length; i++){
-    if(rightMap.has(results[i][0])){
-      const value = rightMap.get(results[i][0]);
-      results[i].push(value);
-    } else {
-      results[i].push(null);
+  if(join === 'left'){
+    for(const [key, value] of leftMap){
+      results.push([key, value]);
+    }
+    // loop through our results array and check if rightMap has the key
+    // if key is found, push the value in rightMap to our nested array at index[i]
+    for(let i = 0; i < results.length; i++){
+      if(rightMap.has(results[i][0])){
+        const value = rightMap.get(results[i][0]);
+        results[i].push(value);
+      } else {
+        results[i].push(null);
+      }
+    }
+  } else { // right join logic
+    for(const [key, value] of rightMap){
+      results.push([key, value]);
+    }
+    for(let i = 0; i < results.length; i++){
+      if(leftMap.has(results[i][0])){
+        const value = leftMap.get(results[i][0]);
+        results[i].push(value);
+      } else {
+        results[i].push(null);
+      }
     }
   }
   return results;
